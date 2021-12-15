60d74e2f05abc21223cea78529c68cbf4527fc9c
Lezd <lilinouxgaming@gmail.com>
2021-12-15 15:14:56 +0100

    Settings sample ListView for Guillaume

--- lib/src/presentation/views/settings_view.dart
+++ lib/src/presentation/views/settings_view.dart
@@ -35,10 +35,46 @@ class SettingsView extends GetView<SettingsViewController> {
         centerTitle: false,
       ),
       body: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF282828),
+          decoration: BoxDecoration(
+            color: Color(0xFF282828),
+          ),
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              InkWell(
+                onTap: () {
+                  Get.to(() => chatSettings());
+                },
+                child: Container(
+                  height: 50,
+                  color: Colors.amber[600],
+                  child: const Center(child: Text('Entry A')),
+                ),
+              ),
+              Container(
+                height: 50,
+                color: Colors.amber[500],
+                child: const Center(child: Text('Entry B')),
+              ),
+              Container(
+                height: 50,
+                color: Colors.amber[100],
+                child: const Center(child: Text('Entry C')),
+              ),
+            ],
+          )),
+    );
+  }
+
+  Widget chatSettings() {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Get.back(),
         ),
       ),
+      body: Container(),
     );
   }
 }
