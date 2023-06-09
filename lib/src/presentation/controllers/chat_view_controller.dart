9c3aee1db4ea5fe6bfae8f7990829a0f01a14e72
Lezd <lilinouxgaming@gmail.com>
2023-06-09 21:46:08 +0900

    fix: selected chat

--- lib/src/presentation/controllers/chat_view_controller.dart
+++ lib/src/presentation/controllers/chat_view_controller.dart
@@ -118,6 +118,8 @@ class ChatViewController extends GetxController
           }
         });
 
+        homeViewController.selectedChat.value = twitchChat;
+
         await applySettings();
       });
     } else {
