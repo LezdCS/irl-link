e49ea2be079604a0f0866855314cac36ce27ba3b
Lezd <xxeurobluexx@gmail.com>
2023-08-14 23:44:58 +0900

    InAppPurchase dialog

--- lib/src/presentation/widgets/settings_view/dialogs/in_app_purchase_dialog.dart
+++ lib/src/presentation/widgets/settings_view/dialogs/in_app_purchase_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../controllers/settings_view_controller.dart';
 
@@ -12,37 +13,101 @@ Widget inAppPurchaseDialog(
       )
       .price;
 
-  return Column(
-    children: [
-      const Text('IRL Link Premium'),
-      Text(price),
-      const Column(
+  return AlertDialog(
+    contentPadding: EdgeInsets.zero,
+    content: Container(
+      height: 400,
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.background,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(8),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Row(
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.check_circle_outline_rounded),
-              Text('StreamElements events view')
-              // todo: preview button show feature screen
+              Text('IRL Link Premium', style: TextStyle(fontSize: 18),),
+              Text(
+                'Please consider subscribing only as a support method as most features are still on development!', 
+                style: TextStyle(fontSize: 13, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text('Features', style: TextStyle(fontSize: 16),),
+              const SizedBox(height: 6,),
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.tertiaryContainer,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        const Icon(Icons.check_circle_outline_rounded, size: 18, color: Colors.green,),
+                        const SizedBox(width: 4,),
+                        Text('StreamElements events view', style: TextStyle(
+                            color: Theme.of(context).textTheme.bodyLarge!.color,
+                            fontSize: 13),)
+                        // todo: preview button show feature screen
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Get.back();
+                },
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 12),
+                  backgroundColor: Theme.of(context).colorScheme.tertiaryContainer,
+                ),
+                child: Text(
+                  "Cancel",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyLarge!.color,
+                      fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(width: 10),
+              TextButton(
+                onPressed: () {
+                  controller.purchase();
+                },
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 12),
+                  backgroundColor: Colors.deepPurpleAccent,
+                ),
+                child: Text(
+                  "Subscribe for $price",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyLarge!.color,
+                      fontSize: 13),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ),
         ],
       ),
-      TextButton(
-        onPressed: () {
-          controller.purchase();
-        },
-        style: TextButton.styleFrom(
-          textStyle: const TextStyle(fontSize: 12),
-          backgroundColor: Colors.deepPurpleAccent,
-        ),
-        child: Text(
-          "Purchase for $price",
-          style: TextStyle(
-              color: Theme.of(context).textTheme.bodyLarge!.color,
-              fontSize: 13),
-          textAlign: TextAlign.center,
-        ),
-      ),
-    ],
+    ),
   );
 }
