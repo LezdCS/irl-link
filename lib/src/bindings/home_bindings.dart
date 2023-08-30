1a03994d6b400ae1f9455877d3bd411ca824a93c
Lezd <xxeurobluexx@gmail.com>
2023-08-30 20:41:50 +0900

    fix home bindings dashboard

--- lib/src/bindings/home_bindings.dart
+++ lib/src/bindings/home_bindings.dart
@@ -7,6 +7,7 @@ import 'package:irllink/src/data/repositories/twitch_repository_impl.dart';
 import 'package:irllink/src/domain/usecases/settings_usecase.dart';
 import 'package:irllink/src/domain/usecases/streamelements_usecase.dart';
 import 'package:irllink/src/domain/usecases/twitch_usecase.dart';
+import 'package:irllink/src/presentation/controllers/dashboard_controller.dart';
 import 'package:irllink/src/presentation/controllers/home_view_controller.dart';
 import 'package:irllink/src/presentation/controllers/obs_tab_view_controller.dart';
 import 'package:irllink/src/presentation/controllers/streamelements_view_controller.dart';
@@ -66,5 +67,8 @@ class HomeBindings extends Bindings {
           ),
         ));
     Get.find<TwitchTabViewController>();
+
+    Get.lazyPut<DashboardController>(() => DashboardController());
+    Get.find<DashboardController>();
   }
 }
