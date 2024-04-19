b64f13c4e6d97c57e48d7a70cd37496eb4cd7bf5
LezdCS <xxeurobluexx@gmail.com>
2024-04-19 14:08:56 +0900

    back support kick events sub subgift host

--- lib/src/presentation/controllers/chat_view_controller.dart
+++ lib/src/presentation/controllers/chat_view_controller.dart
@@ -304,6 +304,13 @@ class ChatViewController extends GetxController
             message.isDeleted = true;
           }
         }
+
+        for (var message in chatMessages.where(
+          (message) =>
+              message.authorId == userId && message.platform == Platform.twitch,
+        )) {
+          message.isDeleted = true;
+        }
         chatMessages.refresh();
       },
       onDeletedMessageByMessageId: (String? messageId) {
@@ -385,45 +392,71 @@ class ChatViewController extends GetxController
       switch (kickEvent?.event) {
         case TypeEvent.message:
           entity.ChatMessage kickMessage = entity.ChatMessage.fromKick(
-              kickEvent as KickMessage,
-              kickChat.userDetails!.userId.toString(),
-              kickChat.userDetails!.subBadges);
+            kickEvent as KickMessage,
+            kickChat.userDetails!.userId.toString(),
+            kickChat.userDetails!.subBadges,
+          );
           chatMessages.add(kickMessage);
           break;
         case TypeEvent.followersUpdated:
-          // TODO: Handle this case.
+          // TODO: TBD
           break;
         case TypeEvent.streamHostEvent:
-          // TODO: Handle this case.
+          entity.ChatMessage kickMessage = entity.ChatMessage.kickHost(
+            kickEvent as KickStreamHost,
+            kickChat.userDetails!.userId.toString(),
+            kickChat.userDetails!.subBadges,
+          );
+          chatMessages.add(kickMessage);
           break;
         case TypeEvent.subscriptionEvent:
-          // TODO: Handle this case.
+          entity.ChatMessage kickMessage = entity.ChatMessage.kickSub(
+            kickEvent as KickSubscription,
+            kickChat.userDetails!.userId.toString(),
+            kickChat.userDetails!.subBadges,
+          );
+          chatMessages.add(kickMessage);
           break;
         case TypeEvent.chatroomUpdatedEvent:
-          // TODO: Handle this case.
+          // TODO: TBD
           break;
         case TypeEvent.userBannedEvent:
-          // TODO: Handle this case.
+          KickUserBanned event = kickEvent as KickUserBanned;
+          for (var message in chatMessages.where(
+            (message) =>
+                message.authorId == event.data.user.id.toString() &&
+                message.platform == Platform.kick,
+          )) {
+            message.isDeleted = true;
+          }
           break;
         case TypeEvent.chatroomClearEvent:
           KickChatroomClear event = kickEvent as KickChatroomClear;
-          chatMessages.removeWhere((message) => message.channelId == event.channel);
+          chatMessages
+              .removeWhere((message) => message.channelId == event.channel);
           break;
         case TypeEvent.giftedSubscriptionsEvent:
-          // TODO: Handle this case.
+          entity.ChatMessage kickMessage = entity.ChatMessage.kickSubGift(
+            kickEvent as KickGiftedSubscriptions,
+            kickChat.userDetails!.userId.toString(),
+            kickChat.userDetails!.subBadges,
+          );
+          chatMessages.add(kickMessage);
           break;
         case TypeEvent.pinnedMessageCreatedEvent:
-          // TODO: Handle this case.
+          // TODO: event in chat (NOT CURRENTLY CODED IN THE APP, SAME FOR TWITCH)
           break;
         case TypeEvent.pollUpdateEvent:
-          // TODO: Handle this case.
+          // TODO: rework poll view to integrate kick polls
           break;
         case TypeEvent.messageDeletedEvent:
           KickMessageDeleted event = kickEvent as KickMessageDeleted;
-          chatMessages.firstWhereOrNull((message) => message.id == event.data.message.id)?.isDeleted = true;
+          chatMessages
+              .firstWhereOrNull(
+                  (message) => message.id == event.data.message.id)
+              ?.isDeleted = true;
           break;
         case null:
-          // TODO: Handle this case.
           break;
       }
 
