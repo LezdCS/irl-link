b34a6533c3d263c40ee5b0e10bb07a2f7db53b94
LezdCS <xxeurobluexx@gmail.com>
2024-08-13 20:32:52 +0900

    remove broken stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:irllink/routes/app_pages.dart';
@@ -48,136 +44,8 @@ void main() async {
   ));
 }
 
-const notificationChannelId = 'irllink_foreground';
-const notificationId = 888;
-const String iosNotificationCategoryPlain = 'irllink_ios_cat';
-
 Future<void> initializeService() async {
-  final service = FlutterBackgroundService();
 
-  const AndroidNotificationChannel channel = AndroidNotificationChannel(
-    notificationChannelId, // id
-    'IRL Link', // title
-    description: 'The app is running in the background.', // description
-    importance: Importance.low, // importance must be at low or higher level
-  );
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  final DarwinInitializationSettings initializationSettingsDarwin =
-      DarwinInitializationSettings(
-    notificationCategories: [
-      DarwinNotificationCategory(
-        iosNotificationCategoryPlain,
-        actions: <DarwinNotificationAction>[
-          DarwinNotificationAction.plain('id1', 'Stop running service'),
-        ],
-        options: <DarwinNotificationCategoryOption>{
-          DarwinNotificationCategoryOption.hiddenPreviewShowTitle,
-        },
-      )
-    ],
-  );
-
-  final InitializationSettings initializationSettings = InitializationSettings(
-    android: const AndroidInitializationSettings('ic_bg_service_small'),
-    iOS: initializationSettingsDarwin,
-  );
-
-  // flutterLocalNotificationsPlugin.initialize(initializationSettings);
-
-  await flutterLocalNotificationsPlugin.initialize(
-    initializationSettings,
-    onDidReceiveBackgroundNotificationResponse: notificationTapBackground,
-  );
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  await service.configure(
-    androidConfiguration: AndroidConfiguration(
-      // this will be executed when app is in foreground or background in separated isolate
-      onStart: onStart,
-
-      // auto start service
-      autoStart: true,
-      isForegroundMode: true,
-
-      notificationChannelId: notificationChannelId,
-      // this must match with notification channel you created above.
-      initialNotificationTitle: 'IRL Link',
-      initialNotificationContent: 'The app is running in the background.',
-      foregroundServiceNotificationId: notificationId,
-    ),
-    iosConfiguration: IosConfiguration(
-      // auto start service
-      autoStart: true,
-
-      // this will be executed when app is in foreground in separated isolate
-      onForeground: onStart,
-
-      // you have to enable background fetch capability on xcode project
-      // onBackground: onIosBackground,
-    ),
-  );
-
-  service.startService();
-}
-
-@pragma('vm:entry-point')
-void onStart(ServiceInstance service) async {
-  // DartPluginRegistrant.ensureInitialized();
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-
-  if (service is AndroidServiceInstance) {
-    if (await service.isForegroundService()) {
-      flutterLocalNotificationsPlugin.show(
-        notificationId,
-        'IRL Link',
-        'The app is running in the background.',
-        const NotificationDetails(
-          android: AndroidNotificationDetails(
-            notificationChannelId,
-            'IRL Link',
-            icon: 'ic_bg_service_small',
-            actions: [
-              AndroidNotificationAction('id1', 'Stop running service'),
-            ],
-          ),
-          iOS: DarwinNotificationDetails(
-            categoryIdentifier: iosNotificationCategoryPlain,
-          ),
-        ),
-      );
-    }
-  }
-
-  if (service is AndroidServiceInstance) {
-    service.on('setAsForeground').listen((event) {
-      service.setAsForegroundService();
-    });
-
-    service.on('setAsBackground').listen((event) {
-      service.setAsBackgroundService();
-    });
-  }
-
-  service.on('stopService').listen((event) {
-    service.stopSelf();
-  });
-}
-
-@pragma('vm:entry-point')
-void notificationTapBackground(NotificationResponse notificationResponse) {
-  if (Platform.isAndroid) {
-    final service = FlutterBackgroundService();
-    service.invoke("stopService");
-  }
 }
 
 class Main extends StatelessWidget {
