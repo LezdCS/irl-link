62740ca2bfee298c4a1c655e716ae8e3894c2207
LezdCS <xxeurobluexx@gmail.com>
2024-09-03 15:29:29 +0900

    edit test script

--- test/twitch_cli_test.sh
+++ test/twitch_cli_test.sh
@@ -6,8 +6,8 @@ twitch event websocket start-server &
 # Function to trigger events
 trigger_events() {
   while true; do
-    # Trigger channel poll begin event
-    twitch event trigger channel.poll.begin --transport=websocket
+    # Trigger channel prediction begin event
+    twitch event trigger channel.prediction.begin --transport=websocket
     # Trigger hype train begin event
     twitch event trigger channel.hype_train.begin --transport=websocket
     # Trigger channel poll begin event again
