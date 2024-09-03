64803e3b0d1aaa9c355471a7ccfd42ef116715cd
LezdCS <xxeurobluexx@gmail.com>
2024-09-03 16:00:52 +0900

    fix

--- test/twitch_cli_api_test.sh
+++ test/twitch_cli_api_test.sh
@@ -1,21 +1,7 @@
 #!/bin/zsh
 
-# Start the Twitch event websocket server
-twitch event websocket start-server &
+# Generate the mock-api data
+twitch mock-api generate
 
-# Function to trigger events
-trigger_events() {
-  while true; do
-    # Trigger channel prediction begin event
-    twitch event trigger channel.prediction.begin --transport=websocket
-    # Trigger hype train begin event
-    twitch event trigger channel.hype_train.begin --transport=websocket
-    # Trigger channel poll begin event again
-    twitch event trigger channel.poll.begin --transport=websocket
-    # Wait for 5 seconds before the next iteration
-    sleep 5
-  done
-}
-
-# Execute the trigger events function
-trigger_events
+# Start the mock-api server
+twitch mock-api start
