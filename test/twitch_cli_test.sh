a0052555fb5ac080bd24ea3c588564acffcb17d7
LezdCS <xxeurobluexx@gmail.com>
2024-09-03 15:27:01 +0900

    test file for twitch cli events

---
+++ test/twitch_cli_test.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# Start the Twitch event websocket server
+twitch event websocket start-server &
+
+# Function to trigger events
+trigger_events() {
+  while true; do
+    # Trigger channel poll begin event
+    twitch event trigger channel.poll.begin --transport=websocket
+    # Trigger hype train begin event
+    twitch event trigger channel.hype_train.begin --transport=websocket
+    # Trigger channel poll begin event again
+    twitch event trigger channel.poll.begin --transport=websocket
+    # Wait for 5 seconds before the next iteration
+    sleep 5
+  done
+}
+
+# Execute the trigger events function
+trigger_events
