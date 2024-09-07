fc6d5b1f260a458de0f701503be624f0b8202fdf
LezdCS <xxeurobluexx@gmail.com>
2024-09-07 16:33:05 +0900

    create watch os target

---
+++ ios/irl-link-watch-os Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  irl-link-watch-os Watch App
+//
+//  Created by Julien on 2024/09/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/irl-link-watch-os Watch App/irl_link_watch_osApp.swift
@@ -0,0 +1,17 @@
+//
+//  irl_link_watch_osApp.swift
+//  irl-link-watch-os Watch App
+//
+//  Created by Julien on 2024/09/07.
+//
+
+import SwiftUI
+
+@main
+struct irl_link_watch_os_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
