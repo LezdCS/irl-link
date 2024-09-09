4662d926054bb27fe540d732d3da11b95539d85e
LezdCS <xxeurobluexx@gmail.com>
2024-09-10 00:55:32 +0900

    Fix link between watchOS and ios app

---
+++ ios/watchkitapp Watch App/watchkitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchkitappApp.swift
+//  watchkitapp Watch App
+//
+//  Created by Julien on 2024/09/10.
+//
+
+import SwiftUI
+
+@main
+struct watchkitapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
