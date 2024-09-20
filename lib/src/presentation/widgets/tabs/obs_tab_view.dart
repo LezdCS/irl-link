8e17294563b47baa239b51fa6b6651a163a81492
LezdCS <xxeurobluexx@gmail.com>
2024-09-20 18:05:30 +0900

    fix

--- lib/src/presentation/widgets/tabs/obs_tab_view.dart
+++ lib/src/presentation/widgets/tabs/obs_tab_view.dart
@@ -233,7 +233,7 @@ class ObsTabView extends GetView<ObsTabViewController> {
         width: 10,
       ),
       itemBuilder: (BuildContext context, int index) {
-        var elementAt = controller.scenesList.elementAt(index);
+        String elementAt = controller.scenesList.elementAt(index);
         return InkWell(
           onTap: () {
             controller.setCurrentScene(elementAt);
@@ -245,7 +245,7 @@ class ObsTabView extends GetView<ObsTabViewController> {
             ),
             alignment: Alignment.center,
             decoration: BoxDecoration(
-              color: controller.currentScene == elementAt
+              color: controller.currentScene.value == elementAt
                   ? Theme.of(context).colorScheme.tertiary
                   : Theme.of(context).colorScheme.tertiaryContainer,
               borderRadius: const BorderRadius.all(Radius.circular(8)),
