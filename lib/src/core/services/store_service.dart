74262624337c9e2dc15c5af8388473248af13d2e
LezdCS <xxeurobluexx@gmail.com>
2024-10-09 18:40:26 +0900

    EULA + Privacy Policy links in purchase dialog

--- lib/src/core/services/store_service.dart
+++ lib/src/core/services/store_service.dart
@@ -35,7 +35,7 @@ class StoreService extends GetxService {
 
   //Function isSubscribed
   bool isSubscribed() {
-    return purchases.any((p) => kIds.contains(p.productID)) || kDebugMode;
+    return purchases.any((p) => kIds.contains(p.productID));
   }
 
   // Function get subscription price
