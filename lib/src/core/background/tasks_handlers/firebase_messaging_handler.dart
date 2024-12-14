24db29073e83f737a97d86ca8599bed21a2ea7f7
LezdCS <xxeurobluexx@gmail.com>
2024-12-14 23:30:28 +0900

    feat: working notifications on android

--- lib/src/core/background/tasks_handlers/firebase_messaging_handler.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
-
-Future<void> handleFirebaseMessaging(RemoteMessage message) async {
-  debugPrint('handleFirebaseMessaging: $message');
-}
