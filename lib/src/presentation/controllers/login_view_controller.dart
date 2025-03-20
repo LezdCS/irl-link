d1cb103e400f4548646bb9d5a1b6738c55b808c5
LezdCS <xxeurobluexx@gmail.com>
2025-03-20 21:06:05 +0900

    fix: internet lookup login

--- lib/src/presentation/controllers/login_view_controller.dart
+++ lib/src/presentation/controllers/login_view_controller.dart
@@ -86,7 +86,7 @@ class LoginViewController extends GetxController {
 
   Future<bool> hasNoNetwork() async {
     try {
-      final result = await InternetAddress.lookup('example.com');
+      final result = await InternetAddress.lookup('irllink.com');
       return !(result.isNotEmpty && result[0].rawAddress.isNotEmpty);
     } on SocketException catch (_) {
       loadingMessage.value = "No internet connection";
